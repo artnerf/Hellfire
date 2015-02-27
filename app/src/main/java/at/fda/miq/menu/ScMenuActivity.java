2266bba4e4ef3daacaf993df86eed0a20ec8afd1
artnerf <franz.artner@firstdata.at>
2015-02-27 16:48:00 +0100

    commit

--- app/src/main/java/at/fda/miq/menu/ScMenuActivity.java
+++ app/src/main/java/at/fda/miq/menu/ScMenuActivity.java
@@ -13,6 +13,8 @@ public class ScMenuActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sc_menu);
+
+
     }
 
 
